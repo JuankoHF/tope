05fd2f07d18fddd74e63d7900e38037594d067ac
JuankoHF <108952217+JuankoHF@users.noreply.github.com>
2023-11-12 17:42:55 +0100

    heder +initial landin en movil small

---
+++ js/class-scrollReveal.js
@@ -0,0 +1,24 @@
+window.sr =ScrollReveal();
+
+    sr.reveal('.header_left', {
+        duration: 2000,
+        origin: 'bottom',
+        distance: '-100px'
+    });
+
+    sr.reveal('.header_right', {
+        duration: 4000,
+    });
+
+    sr.reveal('.get_budget', {
+        duration: 3000,
+        origin: 'top',
+        distance: '-100px'
+    });
+
+    sr.reveal('.tope_etymology', {
+        duration: 2000,
+        delay: 1000,
+        origin: 'left',
+        distance: '-100px'
+    });

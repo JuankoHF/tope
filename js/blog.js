3a90c83d6b5e4bc064c342ebcd3dc8dd23c267a1
JuankoHF <108952217+JuankoHF@users.noreply.github.com>
2023-12-08 21:10:44 +0100

    lista blog from page en movil small

--- js/blog.js
+++ js/blog.js
@@ -6,3 +6,19 @@ function activeNav() {
     navItems.classList.toggle("blog_nav_item_menu");
     spanDown.classList.toggle("span_up")
 }
+
+window.addEventListener('scroll', function() {
+
+    let animateScrollEfect = document.getElementById("scrollEfect");
+    let positionScrollEfect = animateScrollEfect.getBoundingClientRect().top;
+    let boxHeader = document.querySelector(".header_blog_content");
+    console.log(positionScrollEfect)
+
+
+    if(positionScrollEfect < 160) {
+        boxHeader.classList.add("header_box_shadow");
+    }
+    if(positionScrollEfect >= 160) {
+        boxHeader.classList.remove("header_box_shadow");
+    }
+})
